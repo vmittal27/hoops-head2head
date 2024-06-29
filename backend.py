fecdea0dd89400462a606bbd37a7b2dc847137d6
shrivasraghavan <70103199+shrivasraghavan@users.noreply.github.com>
2024-06-29 14:06:38 -0400

    incorporated game modes

--- backend.py
+++ backend.py
@@ -20,13 +20,15 @@ driver = GraphDatabase.driver(uri, auth=(username, password))
 def close_driver():
     driver.close()
 
-def get_two_players():
+def get_two_players(rel_min, rel_max, year_min, year_max):
     """
-    Gets 2 random players
+    Gets 2 random players based on relevancy and year criteria.
+
     """
-    query = """
+    query = f"""
     MATCH (p:Player)
     WITH p, rand() AS r
+    WHERE p.Relevancy >= {rel_min} AND p.Relevancy <= {rel_max} AND p.year >= {year_min} AND p.year <= {year_max}
     RETURN p
     ORDER BY r
     LIMIT 2
@@ -35,9 +37,15 @@ def get_two_players():
         result = session.run(query)
         return [record["p"] for record in result]
 
+
+cur_easy = get_two_players(3.5, float('inf'), 2020, 2024)
+cur_medium = get_two_players(2, 3, 2015, 2024)
+cur_hard = get_two_players(0.5, 1.5, 2015, 2024)
+cur_extreme = get_two_players(1.5, 2.5, 2000, 2024)
+legacy = get_two_players(3.7, float('inf'), 1947, 2024)
 #Grabs two players (returns their node)
 #Adjust later
-rand_players = get_two_players()
+rand_players = cur_easy
 for player in rand_players:
     print(player)
 
