5d7e8022a9da6943311b61858fc6ee31ed07b58c
sriramananth1 <sriraamster@gmail.com>
2024-08-31 13:15:00 -0700

    oops

---
+++ react-frontend/src/pages/Scoreboard.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+function Scoreboard() {
+    console.log('ajskdjf');
+    return (
+        <div>
+            <p> Time's Up!
+                SCOREBOARD: </p>
+        </div>
+    )
+}
+
+export default Scoreboard
+
