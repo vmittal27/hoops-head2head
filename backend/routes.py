714347455437d3f1368c4dde5bd12ac4ecb78336
Viresh Mittal <vireshmittal2@gmail.com>
2024-09-04 21:07:36 -0400

    formatted routes

--- backend/routes.py
+++ backend/routes.py
@@ -2,7 +2,6 @@ import logging
 import random
 from flask import jsonify, request
 from flask_socketio import emit,join_room, leave_room
-from flask_cors import CORS
 from neo4j import GraphDatabase
 from backend import app, socketio
 
@@ -41,11 +40,14 @@ def get_two_players(rel_min, rel_max, year_min, year_max):
     """
     with driver.session() as session:
         result = session.run(query)
-        all_info = [{
-            'player1': record['player1Name'],
-            'player2': record['player2Name'],
-            'shortest_path': record['pathNames']
-        } for record in result]
+        all_info = [
+            {
+                'player1': record['player1Name'],
+                'player2': record['player2Name'],
+                'shortest_path': record['pathNames']
+            } 
+            for record in result
+        ]
         return all_info
     
 @app.route('/')
@@ -78,8 +80,7 @@ def get_players(difficulty):
 
         path = get_shortest_path(p1['id'], p2['id'])
         neighbors = len(path) == 2 # true if direct neighbors, false otherwise
-    
-    {"Player 1": p1, "Player 2": p2, "Path": path}
+
     return {"Player 1": p1, "Player 2": p2, "Path": path}
 
 @app.route("/path/shortest")
@@ -107,7 +108,7 @@ def check_teammates():
     data = request.get_json()
     player1_id = data.get('player1_id')
     player2_id = data.get('player2_id')
-    print(player1_id, player2_id)
+
     if not player1_id or not player2_id:
         return jsonify({'error': 'Missing player names'}), 400
 
@@ -115,6 +116,7 @@ def check_teammates():
     MATCH (a {{`id`: "{player1_id}"}})--(b {{`id`: "{player2_id}"}})
     RETURN COUNT(*) > 0 AS areTeammates
     """
+
     with driver.session() as session:
         result = session.run(query)
         are_teammates = result.single()['areTeammates']
@@ -122,9 +124,12 @@ def check_teammates():
     
 @app.route("/autocomplete")
 def autocomplete_players():
+
     search = request.args.get('search')
+    
     if len(search) < 3:
         return {}
+
     query = f"""
     MATCH (n: Player)
     WHERE toLower(n.name) CONTAINS toLower("{search}")
@@ -134,14 +139,14 @@ def autocomplete_players():
 
     with driver.session() as session:
         result = session.run(query)
-        return {
-            record['id']: [record['year'], record['name']] for record in result
-        }
+        return {record['id']: [record['year'], record['name']] for record in result}
+
 @app.route("/scoring", methods = ['POST'])
 def get_scoring_data():
     data = request.get_json()
     player1_id = data.get('player1_id')
     player2_id = data.get('player2_id')
+
     if not player1_id or not player2_id:
         return jsonify({'error': 'Missing player names'}), 400
 
@@ -150,10 +155,10 @@ def get_scoring_data():
     MATCH (p1)-[r]-(p2)
     RETURN r.weight AS weight, p2.Relevancy AS relevancy
     """
+    
     with driver.session() as session:
         result = session.run(query)
         record = result.single()
-        logging.info('record')
         return {
             'Weight': record["weight"],
             'Relevancy' : record["relevancy"]
@@ -166,6 +171,7 @@ def get_player_json_data():
     '''
     data = request.get_json()
     player_id = data.get('player_id')
+
     if not player_id:
         return jsonify({'error' : 'Missing player id'}), 400
     
@@ -186,18 +192,11 @@ def get_player_json_data():
         else:
             return jsonify({'error': 'Player not found'}), 404
 
-@app.route("/http-call")
-def http_call():
-    """return JSON with string data as the value"""
-    data = {'data':'This text was fetched using an HTTP call to server on render'}
-    return jsonify(data)
-
 @socketio.on("connect")
 def connected():
     """event listener when client connects to the server"""
     print(request.sid)
     print("client has connected")
-    # emit("connect",{"data":f"id: {request.sid} is connected"})
 
 @socketio.on("disconnect")
 def disconnected():
@@ -211,13 +210,14 @@ def disconnected():
                 del rooms[room]
             break
     print("user disconnected")
-    # emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
 
-@socketio.on('data')
+@socketio.on('message')
 def handle_message(data):
     """event listener when client types a message"""
-    print("data from the front end: ",str(data))
-    emit("data",{'data':data,'id':request.sid},broadcast=True)
+    room_id = data['room_id']
+
+    if room_id in rooms:
+        emit("message", {'data': data['message'], 'id': request.sid}, room=room_id)
 
 
 '''
@@ -226,6 +226,7 @@ all room functionality below
 
 rooms = {} 
 game_states = {}
+room_players = {} 
 
 def generate_room_id(n):
     #n digit code, usually use n=6
@@ -253,8 +254,6 @@ def on_join(data):
     else:
         socketio.emit('error', {"message": "Room not found"}, room=request.sid)
 
-room_players = {} #room_id : [list of round info]
-
 @socketio.on('player_finished')
 def player_finished(data):
     room_id = int(data['room_id'])
@@ -272,16 +271,6 @@ def lobby_rejoin(data):
     room_id = int(data['room_id'])
     if room_id in rooms:
         socketio.emit('rejoin_lobby', data, room=room_id)
-    
-# @socketio.on('start_game')
-# def on_start_game(data):
-#     room_id = int(data['room_id'])
-#     if room_id in rooms:
-#         #data should have keys room_id, player1_json, player2_json
-#         player1 = data['player1_json'] #dict, with 'name', 'id', 'url'
-#         player2 = data['player2_json'] #same thing
-#         game_states[room_id] = [player1, player2] 
-#         socketio.emit('game_started', {'room_id': room_id, 'game_data': game_states[room_id]}, room=room_id)
 
 @socketio.on('leave')
 def on_leave(data):
