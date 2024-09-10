3de19e101ff118d7e5c3fc50ea8aa5255b240dfb
Viresh Mittal <vireshmittal2@gmail.com>
2024-09-10 16:10:51 -0400

    created automatic room manager

--- backend/multiplayer.py
+++ backend/multiplayer.py
@@ -1,13 +1,47 @@
 import random
+import threading
+import time
+from datetime import datetime, timedelta
 from flask_socketio import emit,join_room, leave_room
 from backend import app, socketio
 from flask import jsonify, request
 from .neo4j_routes import get_players
 
+CLEANUP_INTERVAL = 600 # cleans rooms every 10 mins
+ROOM_TIMEOUT = 300 # closes rooms after 5 minutes of 0 players
+RUN_CLEANUP_THREAD = True
+
+thread_lock = threading.Lock()
 
 rooms = {} # contains data about the users in each room
 room_players = {} # contains data about the basketball players assigned to each room
 socket_to_user = {} #maps socket id to username
+rooms_usage_tracker: dict[int, datetime] = {} # keeps track of room usage
+
+def generate_room_id(n):
+    #n digit code, usually use n=6
+    return random.randint(10**(n-1)+1, 10**(n))
+
+def update_room_usage(room: int):
+    with thread_lock:
+        rooms_usage_tracker[room] = datetime.now()
+    print(rooms_usage_tracker)
+
+def clean_rooms():
+    while RUN_CLEANUP_THREAD:
+        time.sleep(CLEANUP_INTERVAL)
+        with thread_lock:
+            now = datetime.now()
+            room_ids = list(rooms_usage_tracker.keys())
+            for room_id in room_ids:
+                if now - rooms_usage_tracker[room_id] > timedelta(seconds=ROOM_TIMEOUT) and not rooms[room_id]:
+                    del rooms_usage_tracker[room_id]
+                    del rooms[room_id]
+                    if room_id in room_players:
+                        del room_players[room_id]
+                    print(f"Deleted room {room_id} due to inactivity.")
+cleanup_thread = threading.Thread(target=clean_rooms, daemon=True)
+cleanup_thread.start()
 
 @app.route('/create_room', methods=['POST'])
 def create_room():
@@ -16,7 +50,9 @@ def create_room():
     while room_id in rooms:
         room_id = generate_room_id(6)
 
-    rooms[room_id] = []
+    with thread_lock:
+        rooms[room_id] = []
+    update_room_usage(room=room_id)
  
     return jsonify({"room_id": room_id})
 
@@ -30,14 +66,16 @@ def connected():
 def disconnected():
     """event listener when client disconnects to the server"""
     user_id = request.sid
-    for room in rooms:
-        if user_id in rooms[room]:
-            rooms[room].remove(user_id)
-            socketio.emit('player_left', {"player_count": len(rooms[room]), "players" : rooms[room]}, room=room)
-            if rooms[room] == []:
-                del rooms[room]
-                if room in room_players:
-                    del room_players[room]
+    for room_id in rooms:
+        if user_id in rooms[room_id]:
+            with thread_lock:
+                rooms[room_id].remove(user_id)
+            update_room_usage(room=room_id)
+            socketio.emit('player_left', {"player_count": len(rooms[room_id]), "players" : rooms[room_id]}, room=room_id)
+            # if rooms[room] == []:
+            #     del rooms[room]
+            #     if room in room_players:
+            #         del room_players[room]
             break
     print("user disconnected")
 
@@ -49,10 +87,6 @@ def handle_message(data):
     if room_id in rooms:
         emit("message", {'data': data['message'], 'id': request.sid}, room=room_id)
 
-def generate_room_id(n):
-    #n digit code, usually use n=6
-    return random.randint(10**(n-1)+1, 10**(n))
-
 
 @socketio.on('user_joined') #when a user joins, emit username from the backend
 def on_join(data):
@@ -60,7 +94,9 @@ def on_join(data):
 
     if room_id in rooms:
         join_room(room_id)
-        rooms[room_id].append(request.sid)
+        with thread_lock:
+            rooms[room_id].append(request.sid)
+        update_room_usage(room=room_id)
         socket_to_user[request.sid] = data['username']
         socketio.emit('join_success', {"room_id": room_id, "user_count": len(rooms[room_id])}, room=request.sid)
         socketio.emit('user_joined', {"user_count": len(rooms[room_id]), "users" : rooms[room_id], "user_map" : {socket_id: socket_to_user[socket_id] for socket_id in rooms[room_id]}}, room=room_id)
@@ -92,16 +128,18 @@ def on_leave(data):
     room_id = int(data['room_id'])
     if room_id in rooms and request.sid in rooms[room_id]:
         leave_room(room_id)
-        rooms[room_id].remove(request.sid)
+        with thread_lock:
+            rooms[room_id].remove(request.sid)
+        update_room_usage(room=room_id)
 
-        socketio.emit('leave', {"user_count": len(rooms[room_id]), "users" : rooms[room_id], "user_map" : {socket_to_user[socket_id] for socket_id in rooms[room_id]}}, room=room_id)
+        socketio.emit('leave', {"user_count": len(rooms[room_id]), "users" : rooms[room_id], "user_map" : {socket_id: socket_to_user[socket_id] for socket_id in rooms[room_id]}}, room=room_id)
         del socket_to_user[request.sid]
         
-        if len(rooms[room_id]) == 0:
-            del rooms[room_id]
+        # if len(rooms[room_id]) == 0:
+        #     del rooms[room_id]
 
-            if room_id in room_players:
-                del room_players[room_id]
+        #     if room_id in room_players:
+        #         del room_players[room_id]
 
 @socketio.on('settings_changed')
 def handle_difficulty_change(data):
@@ -134,8 +172,8 @@ def start_game(data):
                 'path': p['Path']
             }
             ppr.append(new_round)
-        
-        room_players[room_id] = ppr
+        with thread_lock:
+            room_players[room_id] = ppr
     socketio.emit('start_game', {"players" : room_players[room_id]}, room=room_id)
 
 @socketio.on("time_changed")
@@ -164,8 +202,10 @@ def handle_reconnect(data):
 
     if room_id in rooms and old_socket_id in rooms[room_id]:
         # Remove the old socket ID and add the new one
-        rooms[room_id].remove(old_socket_id)
-        rooms[room_id].append(new_socket_id)
+        with thread_lock:
+            rooms[room_id].remove(old_socket_id)
+            rooms[room_id].append(new_socket_id)
+        update_room_usage(room=room_id)
 
         # Update the socket_to_user mapping
         if old_socket_id in socket_to_user:
