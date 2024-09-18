c1b142c8e1e3466b277e6312493ed4d577b9d63c
Viresh Mittal <vireshmittal2@gmail.com>
2024-09-18 18:29:40 -0400

    updated landing page

--- react-frontend/src/pages/Homepage.jsx
+++ react-frontend/src/pages/Homepage.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Container, Button, Heading, Image, Box, Text } from '@chakra-ui/react'
+import {React, useState, useEffect} from 'react'
+import { Container, Button, Heading, Image } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
-import { Flex, Spacer, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import { VStack, UnorderedList, ListItem } from '@chakra-ui/react'
+import { Flex, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { UnorderedList, ListItem} from '@chakra-ui/react'
 import {
 	Modal,
 	ModalOverlay,
@@ -15,8 +15,10 @@ import {
     Icon
 } from '@chakra-ui/react'
 import logoImage from '../components/hoopsh2hlogo1-removebg-preview.png'
+import gordonRightImg from '../components/gordon.png'
+import lavineLeftImg from '../components/lavine.png'
 import "../css/Homepage.css"
-import { IconButton, useColorMode, CircularProgress} from "@chakra-ui/react";
+import { IconButton, useColorMode} from "@chakra-ui/react";
 import { MoonIcon, SunIcon, QuestionOutlineIcon } from '@chakra-ui/icons'
 
 
@@ -25,76 +27,132 @@ function Homepage() {
 
     const { isOpen: isRulesOpen , onOpen: onRulesOpen, onClose: onRulesClose } = useDisclosure()
 	const { colorMode, toggleColorMode } = useColorMode();
+
+	const wideScreen = (min) => {
+		const [curWidth, setCurWidth] = useState(0);
+		
+		useEffect(() => {
+		  function handleWindowResize() {
+			setCurWidth(window.innerWidth);
+		  }
+		  
+		  window.addEventListener("resize", (handleWindowResize));
+		  
+		  handleWindowResize();
+		  
+		  return () => { 
+			window.removeEventListener("resize", handleWindowResize);
+		  }
+		}, [setCurWidth]);
+		
+		return curWidth > min;
+	  }
+
     return (
 		<>
-		<Flex minWidth='100%' alignItems='center' justifyContent="flex-end" pt='10px' pr='10px' gap={3}>
-			<IconButton onClick={toggleColorMode} icon={colorMode === 'light' ? <MoonIcon/> : <SunIcon/>}>
-				Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-			</IconButton>
-			
-			<QuestionOutlineIcon onClick={onRulesOpen} className = "rules" boxSize={8} />
+			<Flex minWidth='100%' alignItems='center' justifyContent="space-between" py='10px' px='20px'>
+				<Flex alignItems='center' gap={3}>
+					<a href="/"><Image src={logoImage} boxSize='80px' objectFit='cover'/></a>
+					<Heading fontWeight='bold' fontSize='2xl'>Hoops Head 2 Head</Heading>
+				</Flex>
+				<Flex alignItems='center' gap={3}>
+					<IconButton
+						onClick={toggleColorMode}
+						icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+					>
+						Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+					</IconButton>
+					<QuestionOutlineIcon onClick={onRulesOpen} className="rules" boxSize={8} />
+				</Flex>
+			</Flex>
+			<Container
+				display='flex'
+				flexDirection='column'
+				justifyContent='center'
+				alignItems='center'
+			>
+				<Heading size='xl' textAlign='center' mb='3vh'>Welcome to Hoops Head 2 Head</Heading>
+				<Flex flexDirection='row' justifyContent='center' alignItems='center' gap='5vw'>
+					{wideScreen(800) ? 
+					(
+						<>
+							<Image src={lavineLeftImg} boxSize='30vw'/>
+							<Flex
+								flexDirection='column'
+								width='fit-content'
+								bg='var(--chakra-colors-chakra-subtle-bg)'
+								gap='2em' padding='2em'
+								borderRadius='0.938rem'
+							>
+								<Link to='/singleplayer' ><Button colorScheme='teal' flex='1'size='lg' width='10em' minHeight='4rem'>Single Player</Button></Link>
+								<Link to='/multiplayer'><Button flex='1' size='lg' colorScheme='teal' width='10em' minHeight='4rem'>Multiplayer</Button></Link>
+							</Flex>
+							<Image src={gordonRightImg} boxSize='30vw'/>
+						</>
+					): 
+					(
+						<>
+							<Flex
+								flexDirection='column'
+								width='fit-content'
+								bg='var(--chakra-colors-chakra-subtle-bg)'
+								gap='2em' padding='2em'
+								borderRadius='0.938rem'
+							>
+								<Link to='/singleplayer' ><Button colorScheme='teal' flex='1'size='lg' width='10em' minHeight='4rem'>Single Player</Button></Link>
+								<Link to='/multiplayer'><Button flex='1' size='lg' colorScheme='teal' width='10em' minHeight='4rem'>Multiplayer</Button></Link>
+							</Flex>
+						</>
+					)}
+				</Flex>
 
-		
-		</Flex>
-        <Container className='maincontain'>
-			
-            <Heading pos='relative'>Welcome to Hoops Head 2 Head!</Heading>
-            <Container className='Homepage-Contain'>
-                <Link to='/singleplayer' ><Button flex='1'size='lg' variant='outline' className='Gamemode-Button'>Single Player</Button></Link>
-                <Image flex='1' src={logoImage} boxSize='250' objectFit='cover' />
-                <Link to='/multiplayer'><Button flex='1' size='lg' variant='outline' className='Gamemode-Button'>Multiplayer</Button></Link>
-            </Container>
-            <Button onClick={onRulesOpen} flex='1'size='lg'variant='outline' className='Gamemode-Button'>Rules</Button>
-            <Container>
-            </Container>
-			
-            
-            <Modal blockScrollOnMount={false} size ={'lg'} isOpen={isRulesOpen} onClose={onRulesClose}>
-				<ModalOverlay/>
-				<ModalContent>
-					<ModalHeader>Rules</ModalHeader>
-					<ModalCloseButton color="black"/>
-					<ModalBody>
-						<Tabs variant='enclosed'>
-							<TabList>
-								<Tab>How to Play</Tab>
-								<Tab>Scoring</Tab>
-							</TabList>
+				<Modal blockScrollOnMount={false} size ={'lg'} isOpen={isRulesOpen} onClose={onRulesClose}>
+					<ModalOverlay/>
+					<ModalContent>
+						<ModalHeader>Rules</ModalHeader>
+						<ModalCloseButton color="black"/>
+						<ModalBody>
+							<Tabs variant='enclosed'>
+								<TabList>
+									<Tab>How to Play</Tab>
+									<Tab>Scoring</Tab>
+								</TabList>
 
-							<TabPanels>
-								<TabPanel>
-									<UnorderedList>
-										<ListItem>
-											Connect two NBA players based on mutual teammates
-										</ListItem>
-										<ListItem>
-											Complete the connection in as few guesses as possible
-										</ListItem>
-										<ListItem>
-											The game ends when the connection is complete or you have exhausted all your guesses
-										</ListItem>
-									</UnorderedList>
-								</TabPanel>
-								<TabPanel>
-									<UnorderedList>
-										<ListItem>
-											A higher score is better
-										</ListItem>
-										<ListItem>
-											For correct guesses, obvious teammates add less points than guessing less well-known teammates do
-										</ListItem>
-										<ListItem>
-											Incorrect guesses add 0 points, and each correct guess adds less points if more guesses are used
-										</ListItem>
-									</UnorderedList>
-								</TabPanel>
-							</TabPanels>
-						</Tabs>
-					</ModalBody>
-					<ModalFooter/>
-				</ModalContent>
-			</Modal>
-        </Container>
+								<TabPanels>
+									<TabPanel>
+										<UnorderedList>
+											<ListItem>
+												Connect two NBA players based on mutual teammates
+											</ListItem>
+											<ListItem>
+												Complete the connection in as few guesses as possible
+											</ListItem>
+											<ListItem>
+												The game ends when the connection is complete or you have exhausted all your guesses
+											</ListItem>
+										</UnorderedList>
+									</TabPanel>
+									<TabPanel>
+										<UnorderedList>
+											<ListItem>
+												A higher score is better
+											</ListItem>
+											<ListItem>
+												For correct guesses, obvious teammates add less points than guessing less well-known teammates do
+											</ListItem>
+											<ListItem>
+												Incorrect guesses add 0 points, and each correct guess adds less points if more guesses are used
+											</ListItem>
+										</UnorderedList>
+									</TabPanel>
+								</TabPanels>
+							</Tabs>
+						</ModalBody>
+						<ModalFooter/>
+					</ModalContent>
+				</Modal>
+
+			</Container>
 		</>
         
     )
