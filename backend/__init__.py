6849cd87d66ea2d4f17ed9b152c75f6d9f869099
Viresh Mittal <vireshmittal2@gmail.com>
2024-09-21 16:21:40 -0400

    changed cors policy

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,9 +3,11 @@ __init__ file for the backend package
 '''
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='../react-frontend/dist', static_url_path="/")
+CORS(app, origins=['https://hoopsh2h.sv2projects.com'])
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 from backend import multiplayer
 from backend import neo4j_routes
