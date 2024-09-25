95ce12042244dcbed696381fbf4b1c121068bc72
shrivasraghavan <r.shrivas2004@gmail.com>
2024-09-25 11:24:00 -0400

    added toasts

--- react-frontend/src/components/Difficulty.jsx
+++ react-frontend/src/components/Difficulty.jsx
@@ -6,13 +6,13 @@ import '../css/SinglePlayer.css'
 const DifficultyButton = ({ changeDifficulty, difficulty, roomId, blind, setBlind, isDisabled }) => {
 
     return (  
-      <VStack align="flex-start" spacing={4} className='difficulty-buttons-group'>
+      <VStack className='difficulty-buttons-group'>
         <ButtonGroup className='difficulty-buttons-group'>
             <Button isDisabled={isDisabled} className={difficulty === 'normal' ? "selected-difficulty-button": "difficulty-button"} onClick={() => changeDifficulty('normal')}>Normal</Button>
             <Button isDisabled={isDisabled} className={difficulty === 'hard' ? "selected-difficulty-button": "difficulty-button"} onClick={() => changeDifficulty('hard')}>Hard</Button>
             <Button isDisabled={isDisabled} className={difficulty === 'legacy' ? "selected-difficulty-button": "difficulty-button"} onClick={() => changeDifficulty('legacy')}>Legacy</Button>
         </ButtonGroup>
-        <Heading fontWeight='bold' size='md' m='10px'>Hide Player Names <Switch isDisabled={isDisabled} isChecked={blind} onChange={() => setBlind((oldBlind) => !oldBlind)}></Switch></Heading>
+        <Heading mt='0.5rem' textAlign='center' fontWeight='bold' size='md'>Hide Player Names <Switch isDisabled={isDisabled} isChecked={blind} onChange={() => setBlind((oldBlind) => !oldBlind)}></Switch></Heading>
       </VStack>
     );
 };
